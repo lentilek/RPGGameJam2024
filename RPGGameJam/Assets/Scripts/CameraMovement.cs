4d6b4bdc42ee8f31c047c1aa9d1b73ecb1d90dae
lentilek <laura.robinska@wp.pl>
2024-11-16 10:03:35 +0100

    Level 1 Cyber

--- RPGGameJam/Assets/Scripts/CameraMovement.cs
+++ RPGGameJam/Assets/Scripts/CameraMovement.cs
@@ -12,17 +12,19 @@ public class CameraMovement : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 centerPoints = GetCenterPoint();
-        Vector3 newPosition = centerPoints + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+        //Vector3 newPosition = centerPoints + offset;
+        Vector3 newPosition = new Vector3(centerPoints.x, this.transform.position.y, this.transform.position.z);
+        //transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+        transform.position = newPosition;
     }
 
     private Vector3 GetCenterPoint()
     {
         var bounds = new Bounds(target1.position, Vector3.zero);
-        //bounds.Encapsulate(new Vector3(target1.position.x, 0, 0));
-        //bounds.Encapsulate(new Vector3(target2.position.x, 0, 0));
-        bounds.Encapsulate(target1.position);
-        bounds.Encapsulate(target2.position);
+        bounds.Encapsulate(new Vector3(target1.position.x, this.transform.position.y, this.transform.position.z));
+        bounds.Encapsulate(new Vector3(target2.position.x, this.transform.position.y, this.transform.position.z));
+        //bounds.Encapsulate(target1.position);
+        //bounds.Encapsulate(target2.position);
         return bounds.center;
     }
 }
